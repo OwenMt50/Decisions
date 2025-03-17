6441fc7ded645edb14024efbcfd1d63f1ddfcc57
OwenMt50 <Omartin005@student.butte.edu>
2025-03-17 07:19:15 +0000

    commit

--- Barista.cpp
+++ Barista.cpp
@@ -14,20 +14,20 @@ int main(){
     int whippedcream = 3;
     int user = 0;
 
-    cout<<"Menu: Mocha, Latte, or Espresso.\n";
+    cout<<"Menu: Mocha, Latte, or Espresso.\n"; //Menu for user to choose
     cin>>coffee;
 
-    cout<<"To add one of three extra ingredients type: 1 for milk, 2 for sugar, or 3 for whipped cream.\n";
+    cout<<"To add one of three extra ingredients type: 1 for milk, 2 for sugar, or 3 for whipped cream.\n"; //User chooses ingredient options (only 1)
     cin>>user;
 
-    if(user == 1){
+    if(user == 1){ //If conditions that determine the user's choice 1-3
         order=" with milk.";
     }else if(user==2){
         order=" with sugar.";
     }else if(user==3){
         order=" with whipped cream.";
     }else{
-        order=".";
+        order=" with no extra ingredients."; //If 1-3 is not chosen then they get this message with their coffee choice
     }
 
     cout<<coffee<<""<<order;
